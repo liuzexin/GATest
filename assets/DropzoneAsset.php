276b463804103c021238ceed94ced900b8684a21
liuzexin <liuzexin@sharetimes.cn>
2016-11-08 18:27:11 +0800

    modified:   composer.json

---
+++ assets/DropzoneAsset.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xin
+ * Date: 16/11/8
+ * Time: 下午6:05
+ */
+namespace app\assets;
+
+use yii\web\AssetBundle;
+class DropzoneAsset extends AssetBundle{
+    public $sourcePath = YII_DEBUG?'@bower/dist/':'@bower/dist/min';
+
+    public $js = YII_DEBUG?[
+        'dropzone.js',
+        'dropzone-amd-module.js'
+    ]:[
+        'dropzone.min.js',
+        'dropzone-amd-module.min.js'
+    ];
+
+    public $css = YII_DEBUG?[
+        'basic.css',
+        'dropzone.css'
+    ]:[
+        'dropzone.min.css',
+        'basic.min.css'
+    ];
+}
